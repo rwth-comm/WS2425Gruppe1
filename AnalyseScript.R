6c1d81f9983007e3adcdd04aa1bd952eb0798af4
ToniBre <antonia.breunig@rwth-aachen.de>
2024-12-06 13:11:08 +0100

    Poweranalyse durchgeführt

--- AnalyseScript.R
+++ AnalyseScript.R
@@ -69,4 +69,30 @@ data <- bind_cols(raw.short, scores$scores)
 # Daten exprotieren ---- 
 write_rds(data, "data/data.rds")
 
+install.packages("pwr")
+library(pwr)
 
+# Variante 1: Stichprobengröße gesucht ----
+pwr::pwr.t.test(n = NULL, sig.level = 0.05 , d = 0.8, power = 0.8)
+
+# Variante 2: Signifikanzniveau gesucht ----
+pwr::pwr.t.test(n = 200, sig.level = NULL , d = 0.2, power = 0.8)
+
+# Variante 3: Effektstärke gesucht ----
+pwr::pwr.t.test(n = 110, sig.level = 0.05 , d = NULL, power = 0.8)
+# Es geht in Richtung mittelstarker Effekt, um die Effektstärke detektieren 
+# zu können.
+
+# Variante 4: Power gesucht ----
+
+library(dataforsocialscience)
+library(tidyverse)
+library(lsr)
+df <- robo_care
+df_male <- filter(df, df$gender == "male")
+df_female <- filter(df, df$gender == "female")
+
+t.test(df_male$privacy_concerns, df_female$privacy_concerns)
+cohensD(df_male$privacy_concerns, df_female$privacy_concerns)
+
+pwr::pwr.t.test(n = 122, sig.level = 0.05, d = 0.04400775, power = NULL)
